3e31b7c97057e05223e74bc9be7f570cf1483a0f
asila2104 <79084814+asila2104@users.noreply.github.com>
2022-04-20 19:55:22 +0300

    exceptions update

--- Main.java
+++ Main.java
@@ -3,43 +3,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static String calc(String orLine) { // for calculations
+    public static String calc(String orLine) throws Exception { // for calculations
         String[] line = orLine.split(" "); // string to array [a, op, b]
         String res;
 
         if (line.length != 3) {
-            return "Exception: only 2 numbers and operator!";
+            throw new Exception("only 2 numbers and operator!");
         }
 
         try {
             res = Integer.toString(calcArabic(line)); // try arabic calculations
-            if (res.equals("-1")) { // -1 is wrong operator
-                return "Exception: unknown operator!";
-            }
-            else if (res.equals("-2")) { // -2 is a or b > 10
-                return "Exception: to big numbers!";
-            }
-        } catch (Exception e) { // if not arabic numbers
+        } catch (NumberFormatException e) { // if not arabic numbers
             res = calcRoman(line);
-
-            if (res.equals("Exception")) { // if numbers not roman or numbers > 10
-                return "Exception: different writing systems or invalid input!";
-            }
-            else if (res.equals("Exception2")) { // if result is negative or zero
-                return "Exception: roman numerals do not support negative numbers and zero!";
-            }
         }
 
         return "Result is " + res;
     }
 
-    public static String calcRoman(String[] line) {
+    public static String calcRoman(String[] line) throws Exception {
         int a = romanToArab(line[0]); // roman -> arabic
         int b = romanToArab(line[2]);
         String op = line[1];
 
         if (a == -1 || b == -1) { // if invalid input (or too big numbers)
-            return "Exception";
+            throw new Exception("different writing systems or invalid input!");
         }
 
         int res = switch (op) {
@@ -47,23 +34,23 @@ public class Main {
             case "-" -> a - b;
             case "*" -> a * b;
             case "/" -> a / b;
-            default -> 0;
+            default -> throw new Exception("unknown operator!");
         };
 
         if (res <= 0) { // negative or zero result
-            return "Exception2";
+            throw new Exception("roman numerals do not support negative numbers and zero!");
         }
 
         return arabToRoman(res);
     }
 
-    public static int calcArabic(String[] line) {
+    public static int calcArabic(String[] line) throws Exception {
         int a = Integer.parseInt(line[0]);
         int b = Integer.parseInt(line[2]);
         String op = line[1];
 
         if (a > 10 || b > 10) { // if too big numbers exception
-            return -2;
+            throw new Exception("too big numbers!");
         }
 
         int res = switch (op) {
@@ -71,7 +58,7 @@ public class Main {
             case "-" -> a - b;
             case "*" -> a * b;
             case "/" -> a / b;
-            default -> -1;
+            default -> throw new Exception("unknown operator!");
         };
 
         return res;
@@ -130,7 +117,7 @@ public class Main {
         return res.toString();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine(); //input
 
